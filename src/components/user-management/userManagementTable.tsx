c75f3264d593f4946c1932f68007d42bc2853b70
Momoyaan <ianmathewaviso@gmail.com>
2025-04-08 17:43:55 +0800

    feat: update role options in UserDataTable component for consistency

--- src/components/user-management/userManagementTable.tsx
+++ src/components/user-management/userManagementTable.tsx
@@ -400,7 +400,7 @@ export function UserDataTable() {
                     displayName: "Role",
                     type: "option",
                     icon: UsersIcon,
-                    options: ROLE_OPTIONS,
+                    options: ROLES,
                 }),
                 // --- End Filter Config ---
             },
